017401aea22325eb9945e52573079e9fdaa00f13
Vanek <123vanichka321@gmail.com>
2024-05-02 19:03:24 +0300

    update main.cpp in HashSet

--- HashSet/HashSet/main.cpp
+++ HashSet/HashSet/main.cpp
@@ -5,6 +5,8 @@
 struct Human {
     std::string first_name;
     std::string last_name;
+
+    Human(std::string first, std::string last) : first_name(first), last_name(last) {  };
 };
 
 bool operator==(Human first, Human second) {
@@ -27,32 +29,37 @@ struct std::hash<Human> {
     }
 };
 
+#define ADD(hash_set, set_name, object) \
+    std::cout << "Adding " << object << std::endl; \
+    hash_set.Add(object); \
+    hash_set.Print(set_name); \
+
+#define REMOVE(hash_set, set_name, object) \
+    std::cout << "Removing " << object << std::endl; \
+    hash_set.Remove(object); \
+    hash_set.Print(set_name);
+
+#define CONTAINS(hash_set, set_name, object) \
+    std::cout << object << " contains in " << set_name << "? - "; \
+    if (hash_set.Contains(object)) \
+        std::cout << "YES\n"; \
+    else \
+        std::cout << "NO\n"; \
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    HashSet<Human> humans;
-    humans.Add({ "Алена", "Константиновна" });
-    humans.Print();
-    humans.Add({ "Марк", "Жуков" });
-    humans.Print();
-    humans.Add({ "Кирилл", "Петров" });
-    humans.Print();
-    humans.Add({ "Евгений", "Захаров" });
-    humans.Print();
-    humans.Add({ "Николай", "Злобин" });
-    humans.Print();
+    HashSet<Human> humans{ { "Алена", "Константиновна" }, { "Марк", "Жуков" }, { "Кирилл", "Петров" } };
+    humans.Print("humans");
+    ADD(humans, "humans", Human("Евгений", "Захаров"))
+    ADD(humans, "humans", Human("Николай", "Злобин"))
 
-    std::cout << humans.Contains({ "Николай", "Злобин" });
-    std::cout << humans.Contains({ "ав", "Злобин" });
-    humans.Remove({ "Николай", "Злобин" });
-    std::cout << humans.Contains({ "Николай", "Злобин" });
-    humans.Remove({ "Николай", "Злобин" });
-    humans.Remove({ "Евгений", "Захаров" });
-    humans.Remove({ "Алена", "Константиновна" });
-    humans.Remove({ "Николай", "Злобин" });
-    humans.Print();
+    REMOVE(humans, "humans", Human("Евгений", "Захаров"))
+    REMOVE(humans, "humans", Human("Неопознанный", "Человек"))
 
-    HashSet<int> ints{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 1, 5, 4 };
-    ints.Print();
+    CONTAINS(humans, "humans", Human("Алена", "Константиновна"))
+    CONTAINS(humans, "humans", Human("Евгений", "Захаров"))
+    CONTAINS(humans, "humans", Human("Марк", "Жуков"))
+    CONTAINS(humans, "humans", Human("Неопознанный", "Человек"))
 }
